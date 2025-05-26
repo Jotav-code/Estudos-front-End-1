ccb12b6a67735095dfe6accf18349991670b0085
joao vitor <joaovitor646260@gmail.com>
2025-05-26 19:00:23 -0300

    chore: update polídromo

--- edu.joao.primeirasemana/javascript/Variaveis/polidromo.js
+++ edu.joao.primeirasemana/javascript/Variaveis/polidromo.js
@@ -19,4 +19,39 @@ function varificaPalindromo2(string) {
 	return console.log(true);
 }
 
+
 varificaPalindromo2('asa');
+
+let nome = 'A mala nada na lama'
+
+function verificarPalídromo(string){
+    
+    if(!string) return;
+    
+    return string.replaceAll(" ", "").toLowerCase().split("").reverse().join("") === string.toLowerCase().replaceAll(" ", "")
+    
+}
+console.log(verificarPalídromo(nome))
+
+let nome2 = 'vitor'
+
+function verificarPalídromo(string){
+    
+    if(!string) return;
+    
+    // return string.replaceAll(" ", "").toLowerCase().split("").reverse().join("") === string.toLowerCase().replaceAll(" ", "")
+    
+    
+    let original = string.replaceAll(" ", "").toLowerCase();
+    let nomeVector = original.split("");
+    let aux;
+    for(let i = 0, j = nomeVector.length - 1; i < j; i++, j--){
+        aux = nomeVector[i]
+        nomeVector[i] = nomeVector[j]
+        nomeVector[j] = aux;
+    }
+    let invertida = nomeVector.join("")
+    console.log(original === invertida ? "É um polídromo": "Não é")
+    
+}
+verificarPalídromo(nome2)
